8e7503e0e8adc7a3e11f89e4af74615f714302fd
apiwonska <annapiwonska1@gmail.com>
2021-04-30 20:12:09 +0200

    Add RecipeList

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,4 +1,33 @@
+import { useQuery } from 'react-query';
+import { fetchRecipes } from 'api';
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
+import RecipeItem from '../RecipeItem';
+
 const RecipeList = () => {
-  return <div>Recipe List</div>;
+  const { data, isLoading, isError, error } = useQuery('recipes', fetchRecipes);
+
+  if (isLoading) {
+    return (
+      <Spinner animation="border" role="status" variant="secondary">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error}</Alert>;
+  }
+
+  // console.log(data.records);
+
+  return (
+    <>
+      <h2 className="m-5">Your Recipes!</h2>
+      {data.records.map((recipe) => (
+        <RecipeItem key={recipe.id} recipe={recipe.fields} />
+      ))}
+    </>
+  );
 };
 export default RecipeList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
