524e9828cad61ff53cc23c7e266a1062b1aeb498
apiwonska <annapiwonska1@gmail.com>
2021-06-01 16:39:40 +0200

    Add tests for AddRecipePage component

--- src/pages/AddRecipePage/AddRecipePage.test.tsx
+++ src/pages/AddRecipePage/AddRecipePage.test.tsx
@@ -1,7 +1,242 @@
-describe('AddRecipe', () => {
-  it.todo('adds recipe successfully');
+import { render, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { QueryCache } from 'react-query';
 
-  it.todo('does not submit recipe, data incomplete');
+import { testServerSetup, server, rest } from '__mocks__/testServer';
+import { TestWrapper, getString } from 'shared/testUtils';
+import { getUrl } from 'apis/recipes';
+import AddRecipePage from './AddRecipePage';
 
-  it.todo('submitting fails');
+const WrappedAddRecipePage: React.FC = () => (
+  <TestWrapper>
+    <AddRecipePage />
+  </TestWrapper>
+);
+
+const queryCache = new QueryCache();
+
+// Tests setup
+testServerSetup();
+afterEach(() => {
+  queryCache.clear();
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+describe('AddRecipePage', () => {
+  describe('page layout and initial form state', () => {
+    it('renders all form elements correctly', () => {
+      render(<WrappedAddRecipePage />);
+      const form = screen.getByRole('form');
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+      const linkInput = screen.getByRole('textbox', { name: /link/i });
+      const imageInput = screen.getByRole('textbox', { name: /image/i });
+      const noteInput = screen.getByRole('textbox', { name: /note/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      const cancelButton = screen.getByRole('button', { name: /cancel/i });
+
+      expect(form).toBeInTheDocument();
+      expect(nameInput).toHaveValue('');
+      expect(linkInput).toHaveValue('');
+      expect(imageInput).toHaveValue('');
+      expect(noteInput).toHaveValue('');
+      expect(submitButton).toBeInTheDocument();
+      expect(cancelButton).toBeInTheDocument();
+    });
+
+    it('renders other elements of the page', () => {
+      render(<WrappedAddRecipePage />);
+      const homePageLink = screen.getByRole('link', { name: /home page/i });
+      const header = screen.getByRole('heading', { name: /add new recipe/i });
+
+      expect(homePageLink).toHaveAttribute('href', '/');
+      expect(header).toBeInTheDocument();
+    });
+
+    it('focus is on name input', () => {
+      render(<WrappedAddRecipePage />);
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+
+      expect(nameInput).toHaveFocus();
+    });
+
+    it('submit button is enabled', () => {
+      render(<WrappedAddRecipePage />);
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+
+      expect(submitButton).toBeEnabled();
+    });
+  });
+
+  describe('with valid input (only required fields)', () => {
+    const setup = () => {
+      const utils = render(<WrappedAddRecipePage />);
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+      const linkInput = screen.getByRole('textbox', { name: /link/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      const homePageLink = screen.getByRole('link', { name: /home page/i });
+      user.type(nameInput, 'buritto');
+      user.type(linkInput, 'http://buritto.com');
+      user.click(submitButton);
+      return { ...utils, nameInput, linkInput, submitButton, homePageLink };
+    };
+
+    it('show success message if response status is ok', async () => {
+      setup();
+      const successMessage = await screen.findByText(/recipe was saved/i);
+
+      expect(successMessage).toBeInTheDocument();
+    });
+
+    it('reset form if response status is ok', async () => {
+      const { nameInput, linkInput } = setup();
+
+      await waitFor(() => {
+        expect(nameInput).toHaveValue('');
+        expect(linkInput).toHaveValue('');
+      });
+    });
+
+    it('focus on Go To HomePage link if response status is ok', async () => {
+      const { homePageLink } = setup();
+
+      await waitFor(() => {
+        expect(homePageLink).toHaveFocus();
+      });
+    });
+
+    it('show error message and keep form data if response status is error', async () => {
+      // Switch off logging errors
+      jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
+
+      server.use(
+        rest.post(getUrl('/recipes'), (req, res, ctx) => res(ctx.status(500)))
+      );
+      const { nameInput, linkInput } = setup();
+
+      await waitFor(
+        () => {
+          const errorMessage = screen.getByText(/error occurred/i);
+          expect(errorMessage).toBeInTheDocument();
+        },
+        {
+          timeout: 2000,
+        }
+      );
+
+      expect(nameInput).toHaveValue('buritto');
+      expect(linkInput).toHaveValue('http://buritto.com');
+    }, 10000);
+  });
+
+  describe('with invalid input', () => {
+    it('renders error for empty required fields after on submit', async () => {
+      render(<WrappedAddRecipePage />);
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      user.click(submitButton);
+      const errorName = await screen.findByText(/name is a required field/i);
+      const errorLink = await screen.findByText(/link is a required field/i);
+
+      expect(errorName).toBeInTheDocument();
+      expect(errorLink).toBeInTheDocument();
+    });
+
+    it('renders error if required input is missing', async () => {
+      render(<WrappedAddRecipePage />);
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+      const linkInput = screen.getByRole('textbox', { name: /link/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      user.type(nameInput, ' ');
+      user.type(linkInput, ' ');
+      user.click(submitButton);
+      const errorName = await screen.findByText(/name is a required field/i);
+      const errorLink = await screen.findByText(/link is a required field/i);
+
+      expect(errorName).toBeInTheDocument();
+      expect(errorLink).toBeInTheDocument();
+      expect(nameInput).toHaveAttribute('aria-invalid', 'true');
+      expect(linkInput).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    it('renders error for name shorter than 2', async () => {
+      render(<WrappedAddRecipePage />);
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      user.type(nameInput, 'a');
+      user.click(submitButton);
+      const errorName = await screen.findByText(
+        'name must be at least 2 characters'
+      );
+
+      expect(errorName).toBeInTheDocument();
+      expect(nameInput).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    it('renders error for name longer than 50 characters', async () => {
+      render(<WrappedAddRecipePage />);
+      const nameInput = screen.getByRole('textbox', { name: /name/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+
+      expect(
+        screen.queryByText('name must be at most 50 characters')
+      ).not.toBeInTheDocument();
+
+      user.type(nameInput, getString(51));
+      user.click(submitButton);
+      const errorName = await screen.findByText(
+        'name must be at most 50 characters'
+      );
+
+      expect(errorName).toBeInTheDocument();
+      expect(nameInput).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    it('renders error for note longer than 100 characters', async () => {
+      render(<WrappedAddRecipePage />);
+      const noteInput = screen.getByRole('textbox', { name: /note/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      const errorMessage = 'note must be at most 100 characters';
+
+      expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
+
+      user.type(noteInput, getString(101));
+      user.click(submitButton);
+      const errorNote = await screen.findByText(errorMessage);
+
+      expect(errorNote).toBeInTheDocument();
+      expect(noteInput).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    it('renders error for link if it is not valid url', async () => {
+      render(<WrappedAddRecipePage />);
+      const linkInput = screen.getByRole('textbox', { name: /link/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      const errorMessage = 'link must be a valid URL';
+
+      expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
+
+      user.type(linkInput, 'dsfgfs@ds');
+      user.click(submitButton);
+
+      expect(await screen.findByText(errorMessage)).toBeInTheDocument();
+      expect(linkInput).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    it('renders error for image if it is not valid url', async () => {
+      render(<WrappedAddRecipePage />);
+      const imageInput = screen.getByRole('textbox', { name: /image/i });
+      const submitButton = screen.getByRole('button', { name: /submit/i });
+      const errorMessage = 'image must be a valid URL';
+
+      expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
+
+      user.type(imageInput, 'dsfgfs@ds');
+      user.click(submitButton);
+
+      expect(await screen.findByText(errorMessage)).toBeInTheDocument();
+      expect(imageInput).toHaveAttribute('aria-invalid', 'true');
+    });
+  });
 });
