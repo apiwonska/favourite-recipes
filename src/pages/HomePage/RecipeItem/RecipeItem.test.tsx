5f25ed6eb5d6fa6f0bb5ca884ad43111eb69507b
apiwonska <annapiwonska1@gmail.com>
2021-06-01 16:53:12 +0200

    Add tests of new functionalities of RecipeItem

--- src/pages/HomePage/RecipeItem/RecipeItem.test.tsx
+++ src/pages/HomePage/RecipeItem/RecipeItem.test.tsx
@@ -1,21 +1,80 @@
-import { render, screen } from '@testing-library/react';
+/* disable-eslint react/jsx-props-no-spreading */
+import { render, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { TestWrapper } from 'shared/testUtils';
 
-import RecipeItem from './RecipeItem';
+import recipeData from '__mocks__/validRecipe.json';
+import axiosInstance from 'apis/recipesAxiosInstance';
+import RecipeItem, { RecipeItemProps } from './RecipeItem';
 
-const recipe = {
-  name: 'Grandma Apple Pie',
-  link: 'url',
-  note: 'Best apple pie recipe',
-};
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
-test('renders recipe card', () => {
-  render(<RecipeItem recipe={recipe} />);
+jest.mock('apis/recipesAxiosInstance', () => ({
+  delete: jest.fn(),
+}));
 
-  const title = screen.getByText(recipe.name);
-  const description = screen.getByText(recipe.note);
+window.confirm = jest.fn();
+
+const WrappedRecipeItem: React.FC<RecipeItemProps> = ({ ...props }) => (
+  <TestWrapper>
+    <RecipeItem {...props} />
+  </TestWrapper>
+);
+
+test('renders all elements of recipe card', () => {
+  render(
+    <WrappedRecipeItem recipe={recipeData.fields} recipeId={recipeData.id} />
+  );
+
+  const title = screen.getByText(recipeData.fields.name);
+  const description = screen.getByText(recipeData.fields.note);
   const link = screen.getByText(/go to recipe/i);
+  const image = screen.getByRole('img');
+  const deleteBtn = screen.getByRole('button', { name: /delete/i });
+  const editBtn = screen.getByRole('button', { name: /edit/i });
 
   expect(title).toBeInTheDocument();
   expect(description).toBeInTheDocument();
   expect(link).toBeInTheDocument();
+  expect(image).toHaveAttribute('src', recipeData.fields.image);
+  expect(deleteBtn).toBeInTheDocument();
+  expect(editBtn).toBeInTheDocument();
+});
+
+test('delete card after clicking delete button and user confirmation', async () => {
+  jest
+    .spyOn(axiosInstance, 'delete')
+    .mockImplementation(
+      jest.fn(() =>
+        Promise.resolve({ data: { deleted: 'true', id: `${recipeData.id}` } })
+      )
+    );
+  jest.spyOn(window, 'confirm').mockReturnValueOnce(true);
+  render(
+    <WrappedRecipeItem recipe={recipeData.fields} recipeId={recipeData.id} />
+  );
+  const deleteBtn = screen.getByRole('button', { name: /delete/i });
+  user.click(deleteBtn);
+
+  expect(window.confirm).toBeCalledTimes(1);
+  expect(window.confirm).toHaveReturnedWith(true);
+  await waitFor(() => {
+    expect(axiosInstance.delete).toBeCalledWith(`/recipes/${recipeData.id}`);
+  });
+});
+
+test('go to update recipe page after clicking edit button', () => {
+  render(
+    <WrappedRecipeItem recipe={recipeData.fields} recipeId={recipeData.id} />
+  );
+  const editBtn = screen.getByRole('button', { name: /edit/i });
+
+  user.click(editBtn);
+  expect(mockHistoryPush).toBeCalledWith(`/update/${recipeData.id}`);
 });
